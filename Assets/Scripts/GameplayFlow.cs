2632d0f2bd11a55fe4ef08c1e78cab7e501eb57f
farismiqdad01 <56767913+farismiqdad01@users.noreply.github.com>
2022-10-17 06:05:42 +0700

    HUD base

--- Assets/Scripts/GameplayFlow.cs
+++ Assets/Scripts/GameplayFlow.cs
@@ -8,15 +8,17 @@ public class GameplayFlow : MonoBehaviour
     public void PauseGame()
     {
         Time.timeScale = 0;
-        Debug.Log("Game Paused");
     }
     public void ContinueGame()
     {
         Time.timeScale = 1;
-        Debug.Log("Resume Game!");
     }
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
