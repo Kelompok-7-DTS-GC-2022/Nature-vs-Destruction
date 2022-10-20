08076e7af90d86c2b56b097d910cb639b964a456
farismiqdad01 <56767913+farismiqdad01@users.noreply.github.com>
2022-10-20 16:43:47 +0700

    energy revisi letak

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -10,14 +10,15 @@ public class Energy : MonoBehaviour
     void Start()
     {
         _transform = GetComponent<Transform>();
-        _fallSpeed = Random.Range(10, 20);
+        _fallSpeed = Random.Range(1, 2);
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(transform.position);
         transform.Translate(Vector3.down * _fallSpeed * Time.deltaTime);
-        if (transform.position.y < -20)
+        if (transform.position.y < -2)
         {
             EnergyGone();
         }
