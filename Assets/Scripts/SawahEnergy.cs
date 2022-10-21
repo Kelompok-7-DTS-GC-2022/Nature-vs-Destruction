a2c78557a32a5dd4ac884833bd20ac0d87dea03c
farismiqdad01 <56767913+farismiqdad01@users.noreply.github.com>
2022-10-21 09:35:09 +0700

    animasi sawah

---
+++ Assets/Scripts/SawahEnergy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawahEnergy : MonoBehaviour
+{
+    Animator _animator;
+    [SerializeField] GameObject _sawahEnergyImage;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        InvokeRepeating("CollectEnergyPadi", 3, 3);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _sawahEnergyImage.transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
+    }
+    void CollectEnergyPadi()
+    {
+        EnergyContainer.instance.AddEnergy();
+        _animator.Play("CollectEnergy");
+        print("Collect Energy");
+    }
+}
